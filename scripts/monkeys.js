ea61f088e251334acf0f3e0bf248897962cb1673
Richard Harrington <harrington.richard@gmail.com>
2013-06-12 16:42:27 -0400

    working. probably the last commit

--- scripts/monkeys.js
+++ scripts/monkeys.js
@@ -1,4 +1,5 @@
-;(function() {
+;(function(exports) {
+
     var $ = document.querySelector.bind(document);
     var max = Math.max.bind(Math);
     var min = Math.min.bind(Math);
@@ -7,94 +8,65 @@
     var purposeButtonEl;
     var monkeysButtonEl;
     var monkeysTextEl;
-    var monkeysTextInterval;
-
-    function toggleMonkeysTextColor() {
-        if (monkeysTextEl.classList.contains("alt-color")) {
-            monkeysTextEl.classList.remove("alt-color");
-            monkeysTextEl.style.color = "#fa430b";
-        }
-        else {
-            monkeysTextEl.classList.add("alt-color");
-            monkeysTextEl.style.color = "#380bfa";
-        }
-    }
-
-    function monkeysGoCleanup() {
-        monkeysButtonEl.removeEventListener("click", monkeysGo, false);
-        clearInterval(monkeysTextInterval);
-    }
-
-    function monkeysStopCleanup() {
-        monkeysButtonEl.removeEventListener("click", monkeysStop, false);
-    }
 
     function monkeysGo() {
-        monkeysStopCleanup();
+        monkeysButtonEl.removeEventListener("click", monkeysGo, false);
         monkeysTextEl.style.display = "block";
-        monkeysTextInterval = setInterval(function() {
-            toggleMonkeysTextColor();
-        }, 750);
         monkeysButtonEl.innerHTML = "Click here to stop the monkeys";
         monkeysButtonEl.addEventListener("click", monkeysStop, false);
     }
 
     function monkeysStop() {
-        monkeysGoCleanup();
+        monkeysButtonEl.removeEventListener("click", monkeysStop, false);
         monkeysTextEl.style.display = "none";
         monkeysButtonEl.innerHTML = "Click here to reveal monkeys";
         monkeysButtonEl.addEventListener("click", monkeysGo, false);
     }
 
-    function getMaxEscapeDistances(elBounds) {
-        var windowBounds = {
-            top:    0,
-            left:   0,
-            bottom: window.innerHeight,
-            right:  window.innerWidth
-        };
-        var result = {};
-        for (var dir in windowBounds) {
-            result[dir] = windowBounds[dir] - elBounds[dir];
-        }
-        return result;
-    }
-
-    function getEncroachingVector(elBounds, mousePos) {
-        var center = {
-            x: elBounds.left + (elBounds.right - elBounds.left) / 2,
-            y: elBounds.top + (elBounds.bottom - elBounds.top) / 2
-        };
-        var result = {
-            x: mousePos.x < center.x ? 
-                    max(0, mousePos.x - elBounds.left) :
-                    min(0, mousePos.x - elBounds.right),
-            y: mousePos.y < center.y ?
-                    max(0, mousePos.y - elBounds.top) :
-                    min(0, mousePos.y - elBounds.bottom)
-        };
-        return  result;
-    }
-
-    function getEscapeVector(maxEscapeDistances, encroachingVector) {
-        var result = {
-            x: encroachingVector.x > 0 ?
-                    min(maxEscapeDistances.right, encroachingVector.x) :
-                    max(maxEscapeDistances.left, encroachingVector.x),
-            y: encroachingVector.y > 0 ?
-                    min(maxEscapeDistances.bottom, encroachingVector.y) :
-                    max(maxEscapeDistances.top, encroachingVector.y)
-        };
-        return result;
-    }
-
-    function resetPurposeButton() {
-        purposeButtonContainerEl.style.left = "0px";
-        purposeButtonContainerEl.style.top = "0px";
-        monkeysGo();
-    }
-
     function movePurposeButton(event) {
+
+        function getMaxEscapeDistances(elBounds) {
+            var windowBounds = {
+                top:    0,
+                left:   0,
+                bottom: window.innerHeight,
+                right:  window.innerWidth
+            };
+            var result = {};
+            for (var dir in windowBounds) {
+                result[dir] = windowBounds[dir] - elBounds[dir];
+            }
+            return result;
+        }
+
+        function getEncroachingVector(elBounds, mousePos) {
+            var center = {
+                x: elBounds.left + (elBounds.right - elBounds.left) / 2,
+                y: elBounds.top + (elBounds.bottom - elBounds.top) / 2
+            };
+            var result = {
+                x: mousePos.x < center.x ? 
+                        max(0, mousePos.x - elBounds.left) :
+                        min(0, mousePos.x - elBounds.right),
+                y: mousePos.y < center.y ?
+                        max(0, mousePos.y - elBounds.top) :
+                        min(0, mousePos.y - elBounds.bottom)
+            };
+            return  result;
+        }
+
+        function getEscapeVector(maxEscapeDistances, encroachingVector) {
+            var result = {
+                x: encroachingVector.x > 0 ?
+                        min(maxEscapeDistances.right, encroachingVector.x) :
+                        max(maxEscapeDistances.left, encroachingVector.x),
+                y: encroachingVector.y > 0 ?
+                        min(maxEscapeDistances.bottom, encroachingVector.y) :
+                        max(maxEscapeDistances.top, encroachingVector.y)
+            };
+            return result;
+        }
+
         var elBounds = purposeButtonContainerEl.getBoundingClientRect();
         var mousePos = {
             x: event.clientX,
@@ -111,6 +83,12 @@
         purposeButtonContainerEl.style.top = newTop;
     }
 
+    function resetPurposeButton() {
+        purposeButtonContainerEl.style.left = "0px";
+        purposeButtonContainerEl.style.top = "0px";
+        monkeysGo();
+    }
+
     document.addEventListener('DOMContentLoaded', function() {
         purposeButtonContainerEl = $('.purpose-button-container');
         purposeButtonEl = $('.purpose-button');
@@ -120,6 +98,16 @@
         monkeysButtonEl.addEventListener("click", monkeysGo, false);
         purposeButtonEl.addEventListener("click", resetPurposeButton, false);
         purposeButtonContainerEl.addEventListener("mousemove", movePurposeButton, false);
+
+
     }, false);
 
-})();
+
+setTimeout (function() {
+    exports.monkeysGo = monkeysGo;
+    exports.monkeysStop = monkeysStop;
+    exports.monkeysButtonEl = monkeysButtonEl;
+
+},1000);
+
+}(this));
